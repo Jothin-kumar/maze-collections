7ccc450207ef00529826df8e94fb40bb023406a0
Jothin Kumar <contact@jothin.tech>
2024-04-10 20:48:36 +0530

    build

---
+++ build.sh
@@ -0,0 +1,21 @@
+if [ -d "build" ]
+then
+    cd build
+    git pull
+    cd ../
+else
+    git clone https://github.com/Jothin-kumar/build.git
+fi
+printf "git (build) ✅\n\n"
+
+python3 make.py
+printf "make.py ✅\n\n"
+
+python3 build/build.py
+printf "build.py ✅\n\n"
+
+cp -r maze build-output/maze
+printf "maze data ✅\n\n"
+
+cp _redirects build-output/_redirects
+printf "_redirects ✅\n\n"
