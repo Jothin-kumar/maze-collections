6d9d861ffce0970cc3ba244187c66032de43417a
Jothin Kumar <contact@jothin.tech>
2024-04-11 06:19:20 +0530

    repair maze py

--- repair-maze.py
+++ repair-maze.py
@@ -1,6 +1,9 @@
-with open("mazes.txt", "w+") as f:
+with open("mazes.txt", "r") as f:
     c = f.read()
-    while "\n\n" in c:
-        c = c.replace("\n\n", "\n")
-    c = c.replace("\"", "")
+
+while "\n\n" in c:
+    c = c.replace("\n\n", "\n")
+c = c.replace("\"", "")
+
+with open("mazes.txt", "w") as f:
     f.write(c)
