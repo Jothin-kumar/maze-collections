f78bca7647a83bb6f4bf89cae425fa34d45559c9
Jothin Kumar <contact@jothin.tech>
2024-04-11 06:27:30 +0530

    remove last blank line

--- repair-maze.py
+++ repair-maze.py
@@ -4,6 +4,7 @@ with open("mazes.txt", "r") as f:
 while "\n\n" in c:
     c = c.replace("\n\n", "\n")
 c = c.replace("\"", "")
+c = c.removesuffix("\n")
 
 with open("mazes.txt", "w") as f:
     f.write(c)
