d511e3aca19419c7b4e3d45c92351cbe1a3a88b8
Jothin Kumar <contact@jothin.tech>
2024-05-07 16:23:14 +0530

    loading and error msg

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const level = usp.get('level') || 'easy';
 async function bodyLoaded() {
     document.getElementById('lvl-' + level).classList.add('current-lvl');
     const main = document.getElementById('main');
+    const msgElem = document.getElementById('main-msg');
     const r = await fetch('/maze/max.txt');
+    if (!r.ok) {
+        msgElem.innerText = 'Failed to load mazes';
+        msgElem.style.color = 'red';
+        return;
+    }
     const max = parseInt(await r.text());
     for (let i = 1; i < max+1; i++) {
         main.appendChild(newMaze(i));
     }
+
+    msgElem.style.display = 'none';
 }
 
 function newMaze(mazeId) {
