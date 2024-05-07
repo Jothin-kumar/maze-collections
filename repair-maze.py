3cde5098ae98f50bd94ae514a84dc15273001100
Jothin Kumar <contact@jothin.tech>
2024-05-07 17:09:01 +0530

    repair maze data

--- repair-maze.py
+++ repair-maze.py
@@ -1,10 +1,11 @@
-with open("mazes.txt", "r") as f:
-    c = f.read()
+for lvl in ['easy', 'medium', 'hard']:
+    with open(f"maze-data/{lvl}.txt", "r") as f:
+        c = f.read()
 
-while "\n\n" in c:
-    c = c.replace("\n\n", "\n")
-c = c.replace("\"", "")
-c = c.removesuffix("\n")
+    while "\n\n" in c:
+        c = c.replace("\n\n", "\n")
+    c = c.replace("\"", "")
+    c = c.removesuffix("\n")
 
-with open("mazes.txt", "w") as f:
-    f.write(c)
+    with open(f"maze-data/{lvl}.txt", "w") as f:
+        f.write(c)
