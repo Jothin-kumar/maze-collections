25e00e42d039527aa37d07fef4016e367c12a0a2
Jothin Kumar <contact@jothin.tech>
2024-05-07 16:31:36 +0530

    make for all 3 levels

--- make.py
+++ make.py
@@ -2,11 +2,15 @@ from os import mkdir, path
 
 if not path.exists('maze'):
     mkdir('maze')
+    mkdir('maze/easy')
+    mkdir('maze/medium')
+    mkdir('maze/hard')
 
-with open("mazes.txt") as f:
-    for i, line in enumerate(f.readlines()):
-        with open(f"maze/{i+1}.txt", "w") as f2:
-            f2.write(line)
+for lvl in ['easy', 'medium', 'hard']:
+    with open(f"maze-data/{lvl}.txt") as f:
+        for i, line in enumerate(f.readlines()):
+            with open(f"maze/{lvl}/{i+1}.txt", "w") as f2:
+                f2.write(line)
 
-with open("maze/max.txt", "w") as f:
-    f.write(str(i+1))
+    with open(f"maze/{lvl}/max.txt", "w") as f:
+        f.write(str(i+1))
