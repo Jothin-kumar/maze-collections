37341248fda80307de4efe3d321629ec594ab454
Jothin Kumar <contact@jothin.tech>
2024-05-09 11:38:24 +0530

    fix import error and newline issue

--- py/make.py
+++ py/make.py
@@ -1,5 +1,5 @@
 from os import mkdir, path
-from .validate import is_valid as maze_is_valid
+from validate import is_valid as maze_is_valid
 
 if not path.exists('maze'):
     mkdir('maze')
@@ -11,6 +11,7 @@ for lvl in ['easy', 'medium', 'hard']:
     with open(f"maze-data/{lvl}.txt") as f:
         for i, line in enumerate(f.readlines()):
             with open(f"maze/{lvl}/{i+1}.txt", "w") as f2:
+                line = line.removesuffix("\n")
                 assert maze_is_valid(line)
                 f2.write(line)
 
