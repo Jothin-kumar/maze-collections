89c1729505e323d9d3cfd74f5010b1510f62fbed
Jothin Kumar <contact@jothin.tech>
2024-05-09 11:31:11 +0530

    check maze before adding it

--- py/make.py
+++ py/make.py
@@ -1,4 +1,5 @@
 from os import mkdir, path
+from .validate import is_valid as maze_is_valid
 
 if not path.exists('maze'):
     mkdir('maze')
@@ -10,6 +11,7 @@ for lvl in ['easy', 'medium', 'hard']:
     with open(f"maze-data/{lvl}.txt") as f:
         for i, line in enumerate(f.readlines()):
             with open(f"maze/{lvl}/{i+1}.txt", "w") as f2:
+                assert maze_is_valid(line)
                 f2.write(line)
 
     with open(f"maze/{lvl}/max.txt", "w") as f:
