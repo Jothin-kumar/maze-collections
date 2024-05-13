14546526ce16846631ac4017b1227e7fcee5ce0b
Jothin Kumar <contact@jothin.tech>
2024-05-13 17:03:06 +0530

    Don't allow duplicate mazes (#11)
    * Don't allow duplicate mazes
    Fixes #9
    
    * sample duplicate (this should fail)
    
    * this build should fail
    
    * this build should be successfull
    
    * remove accidentally added submodule
    
    * this should fail
    
    * this should succeed

--- py/make.py
+++ py/make.py
@@ -9,9 +9,10 @@ if not path.exists('maze'):
 
 for lvl in ['easy', 'medium', 'hard']:
     with open(f"maze-data/{lvl}.txt") as f:
-        for i, line in enumerate(f.readlines()):
+        lines = [s.removesuffix("\n") for s in f.readlines()]
+        assert len(lines) == len(set(lines)), f"Duplicate mazes in {lvl}.txt"
+        for i, line in enumerate(lines):
             with open(f"maze/{lvl}/{i+1}.txt", "w") as f2:
-                line = line.removesuffix("\n")
                 assert maze_is_valid(line, lvl), f"Invalid maze: {i+1} in {lvl} level"
                 f2.write(line)
 
