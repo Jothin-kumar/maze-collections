8a3e0dfddb802206f9b9087ac9fb38a31414ed37
Jothin Kumar <contact@jothin.tech>
2024-05-19 11:17:36 +0530

    no longer needed

--- build.sh
+++ build.sh
@@ -25,8 +25,5 @@ printf "maze data ✅\n\n"
 cp stats.txt build-output/stats.txt
 printf "stats.txt ✅\n\n"
 
-cp root/_redirects build-output/_redirects
-printf "_redirects ✅\n\n"
-
 cp root/robots.txt build-output/robots.txt
 printf "robots.txt ✅\n\n"
